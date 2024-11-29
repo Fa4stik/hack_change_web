d987b860460a975673679acca8e9683b4783e0dd
Fa4stik <stepan.sharifulin@mail.ru>
2024-11-29 19:05:10 +0500

    Update - Config vite for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,45 @@
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react-swc'
-import * as path from "node:path";
+import * as path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      include: '**/*.svg?react',
-      svgrOptions: {
-        svgoConfig: {
-          plugins: [
-            'removeXMLNS',
-            {
-              name: 'removeAttrs',
-              params: {
-                attrs: 'path:fill',
-              },
-            },
-            {
-              name: 'preset-default',
-            },
-          ],
-        },
-      },
-    }),
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
-  build: {
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+	plugins: [
+		react(),
+		svgr({
+			include: '**/*.svg?react',
+			svgrOptions: {
+				svgoConfig: {
+					plugins: [
+						'removeXMLNS',
+						{
+							name: 'removeAttrs',
+							params: {
+								attrs: 'path:fill',
+							},
+						},
+						{
+							name: 'preset-default',
+						},
+					],
+				},
+			},
+		}),
+	],
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
+	},
+	build: {
+		outDir: 'dist',
+		assetsDir: 'assets',
+		sourcemap: true,
+	},
+	base: './',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
 })
