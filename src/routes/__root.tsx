3a3ee24c29d20ac827ea63c3c4b910fc6bba89f4
Fa4stik <stepan.sharifulin@mail.ru>
2024-11-30 12:35:03 +0500

    Update - Logic navigation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Outlet, createRootRoute, useNavigate } from '@tanstack/react-router'
+import { useEffect } from 'react'
 
 export const Route = createRootRoute({
 	component: RootComponent,
 })
 
 function RootComponent() {
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		const handleKeyDown = (e: KeyboardEvent) => {
+			e.preventDefault()
+
+			if (e.ctrlKey && e.code === 'KeyL') {
+				navigate({ to: '/' })
+			}
+
+			if (e.ctrlKey && e.code === 'KeyD') {
+				navigate({ to: '/structure/table' })
+			}
+
+			if (e.ctrlKey && e.code === 'KeyG') {
+				navigate({ to: '/structure/graph' })
+			}
+		}
+
+		window.addEventListener('keydown', handleKeyDown)
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown)
+		}
+	}, [])
+
 	return (
 		<React.Fragment>
 			<Outlet />
